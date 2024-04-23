samsung-msm8974/android_device_samsung_hlte

Add device soong namespace

BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/hlte-include
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_hlte
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_hlte
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineage_hlte_bcm2079x_defconfig

samsung-msm8974/android_device_samsung_kltechn

BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../$(COMMON_PATH)/recovery/recovery_keys.
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/klte-include
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_kltechn
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_kltechn
 
 # Fingerprint
 include $(COMMON_PATH)/fingerprint/board.mk